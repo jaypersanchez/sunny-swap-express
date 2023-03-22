80f9738d38b6697066b4cdca26a4202232d8b8eb
Jayper Sanchez <jaypersanchez@gmail.com>
2023-03-22 14:27:27 +0800

    initial save

---
+++ test/swappertest.js
@@ -0,0 +1,5 @@
+const NftSwap = '@traderxyz/nft-swap-sdk';
+
+// From your app, provide NftSwap the web3 provider, signer for the user's wallet, and the chain id.
+//const nftSwapSdk = new NftSwap(provider, signer, chainId);
+console.log(`Swapper Test`)
